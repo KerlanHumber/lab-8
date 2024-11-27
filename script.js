954695d32f979cd71b5dc8b8e22f378fdaa37aa3
Kerlan Augustine <kerlanhumber@gmail.com>
2024-11-27 18:11:28 -0500

    Compared the two objects

--- script.js
+++ script.js
@@ -26,3 +26,8 @@ console.log("JSON string:", studentJSON);
 // Converting the JSON string back to JavaScript
 const studentObjectFromJSON = JSON.parse(studentJSON);
 console.log("Object from JSON:", studentObjectFromJSON);
+
+// Comparint the two objects
+console.log("Are they equal?", student1 === studentObjectFromJSON);
+
+
