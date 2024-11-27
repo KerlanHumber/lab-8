5e09b88cfb6829034d4d7335894aa3e6324f1e15
Kerlan Augustine <kerlanhumber@gmail.com>
2024-11-27 13:31:58 -0500

    Implemented part 1

---
+++ script.js
@@ -0,0 +1,20 @@
+class Student {
+    constructor(name, age, enrolled, courses){
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.courses = courses;
+    }
+
+    displayInfo(){
+        console.log(`Name: ${this.name}, Age: ${this.age}`);
+    }
+}
+
+const student1 = new Student("Alice", 20,["Math", "Science", "History"]);
+
+
+console.log("Name:", student1.name);
+console.log("Age:", student1.age);
+
+student1.displayInfo();
